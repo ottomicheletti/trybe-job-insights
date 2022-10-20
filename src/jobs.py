0f3a1a65d11e01934e86b33826106e7c8ab916b1
ottomicheletti <michelettiotto@gmail.com>
2022-10-20 17:54:33 -0300

    feat(Req 01): :sparkles: Implements the function read() on jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,13 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
-def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+def read(path: str):
+    result = []
+    with open(path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            result.append(row)
+    print(result[0])
+    return result
