85c86757ee24d8e131232a8b9e9b801d5361553a
ottomicheletti <michelettiotto@gmail.com>
2022-10-21 22:52:50 -0300

    test(Req 10): :white_check_mark: Implements the test_counter() function within test_counter.py

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+
+path = "src/jobs.csv"
 
 
 def test_counter():
-    pass
+    assert (
+        count_ocurrences(path, "Python") == 1639
+    ), "Expected 1639 entries for the word Python"
+    assert (
+        count_ocurrences(path, "Java") == 676
+    ), "Expected 676 entries for the word Java"
+    assert (
+        count_ocurrences(path, "React") == 141
+    ), "Expected 141 entries for the word React"
