ff10208e19ddb8779fee652d6f58edc606a9df13
ottomicheletti <michelettiotto@gmail.com>
2022-10-21 23:12:51 -0300

    feat(Req 11): :white_check_mark: Implements test_brazilian_jobs() function within test_brazilian_jobs.py

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file(path)[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
